0812e9f2101d6872cf07bbdf692d5e4d5280d307
pradivgnanaraj <tpradivgnanaraj@gmail.com>
2023-07-14 21:34:20 +0530

    Minor bug fixing in functions.py Implement of Edit button

--- gui.py
+++ gui.py
@@ -4,24 +4,44 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter ToDo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(),
+                      key="todos",
+                      enable_events=True,
+                      size=[45, 10]
+                      )
+edit_button = sg.Button("Edit")
 
 window = sg.Window("My To-Do App",
                    layout=[[label],
-                           [input_box, add_button]],
-                   font=('Helvitica', 20)
+                           [input_box, add_button],
+                           [list_box, edit_button]],
+                   font=('Helvetica', 20)
                    )
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
     match event:
         case 'Add':
             todos = functions.get_todos()
             new_todo = values['todo']
             todos.append(new_todo)
             functions.write_todos(todos)
+
+        case 'Edit':
+            todo_to_edit = values['todos']
+            new_todo = values['todo']
+            todos = functions.get_todos()
+            todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(values=values['todos'][0])
         case sg.WIN_CLOSED:
             break
+
 
 window.close()
 
--- modules/functions.py
+++ modules/functions.py
@@ -17,9 +17,9 @@ def write_todos(todos_arg, filepath=FILEPATH):
     :return:
     """
     with open(filepath, 'w') as file:
-        file.writelines(todos)
+        file.writelines(todos_arg)
 
 
-if __name__=="__cli__":
+if __name__ == "__cli__":
     print("Hello")
     print(get_todos())
