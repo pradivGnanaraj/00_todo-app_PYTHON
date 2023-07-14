eead22c876b3ca040e75507a7999b51a6fdd88f4
pradivgnanaraj <tpradivgnanaraj@gmail.com>
2023-07-14 16:22:56 +0530

    added GUI initial.

---
+++ gui.py
@@ -0,0 +1,16 @@
+from modules import functions
+import PySimpleGUI as sg
+
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter ToDo")
+add_button = sg.Button("Add")
+
+window = sg.Window("My To-Do App",
+                   layout=[[label],
+                           [input_box, add_button]]
+                   )
+window.read()
+window.close()
+
+
+
