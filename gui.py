fa63a91a7bc33a87fbe46d741e91f52c2158f3a9
pradivgnanaraj <tpradivgnanaraj@gmail.com>
2023-07-18 22:08:51 +0530

    Bug Fix : Edit button real time update in GUI

--- gui.py
+++ gui.py
@@ -4,11 +4,18 @@ import PySimpleGUI as PySG
 label = PySG.Text("Type in a to-do")
 input_box = PySG.InputText(tooltip="Enter todo", key="todo")
 add_button = PySG.Button("Add")
+list_box = PySG.Listbox(values=functions.get_todos(),
+                        key='todos',
+                        enable_events=True,
+                        size=[45, 10]
+                        )
+edit_button = PySG.Button("Edit")
 
 window = PySG.Window("My To-Do App",
                      layout=[
                          [label],
-                         [input_box, add_button]
+                         [input_box, add_button],
+                         [list_box, edit_button]
                      ],
                      font=('Helvetica', 20)
                      )
@@ -24,8 +31,22 @@ while True:
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
 
         case PySG.WIN_CLOSED:
             break
+
 
 window.close()
